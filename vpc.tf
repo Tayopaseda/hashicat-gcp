84386e682b13dabad7d4ff6062fbcf875fca3d97
tayo <tayo-p@live.co.uk>
2021-11-11 12:26:43 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,19 @@
+module "network" {
+  source  = "app.terraform.io/cognizant-QA-demo/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  network_name = "gaurav-network"
+  project_id =var.project
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
+
+
 module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.0"
